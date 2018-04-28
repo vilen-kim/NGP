42f751400d22062f46c2b6cea82cd79dedc85d8c
vilen-kim <vilen_kim@mail.ru>
2018-04-28 16:28:22 +0500

    Прикрутил карту Google

--- web/js/ckeditor.js
+++ web/js/ckeditor.js
@@ -1,1 +1,4 @@
-//CKEDITOR.plugins.addExternal('filebrowser', '/plugins/filebrowser/plugin.js', '');
+//CKEDITOR.plugins.addExternal('widget', '/plugins/widget/plugin.js', '');
+//CKEDITOR.plugins.addExternal('widgetselection', '/plugins/widgetselection/plugin.js', '');
+//CKEDITOR.plugins.addExternal('lineutils', '/plugins/lineutils/plugin.js', '');
+//CKEDITOR.plugins.addExternal('locationmap', '/plugins/locationmap/plugin.js', '');
