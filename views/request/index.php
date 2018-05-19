3e1e57af5b23dd94ccf5821c9e22277d1784eca2
vilen-kim <vilen_kim@mail.ru>
2018-05-19 20:36:21 +0500

    Экстренная починка вывода обращений в админке

--- views/request/index.php
+++ views/request/index.php
@@ -15,13 +15,24 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php
     echo GridView::widget([
         'dataProvider' => $dataProvider,
-        //'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
             [
                 'attribute' => 'request_auth_id',
                 'content' => function ($model, $key, $index, $column) {
-                    return Html::a($model->requestAuth->fio . ' - ' . $model->requestAuth->executive->position, ['auth/view', 'id' => $model->requestAuth->id]);
+                    $result = '';
+                    if ($model->requestAuth){
+                        $result .= $model->requestAuth->fio;
+                    }
+                    if ($model->requestAuth->executive){
+                        $result .= ' - ' . $model->requestAuth->executive->position;
+                    }
+                    if ($result){
+                        return Html::a($result, ['auth/view', 'id' => $model->requestAuth->id]);
+                    } else {
+                        return '';
+                    }
+                        
                 },
             ],
             [
