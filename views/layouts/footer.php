79c2f3bda49d3173d8cf64ef8f4b53854750fa9d
vilen-kim <vilen_kim@mail.ru>
2018-08-29 15:12:52 +0500

    Сделан футер

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -2,44 +2,51 @@
     use yii\helpers\Html;
 ?>
 
-<footer class="rows">
-    <div class="col-md-3">
-        <div class="mediumWhiteBold">Наименование</div>
-        <div class="smallWhite">Бюджетное учреждение Ханты-Мансийского автономного округа - Югры "Няганская городская поликлиника"</div>
-    </div>
-    <div class="col-md-2">
-        <div class="mediumWhiteBold">О поликлинике</div>
-    </div>
-    <div class="col-md-2">
-        <div class="mediumWhiteBold">Пациентам</div>
-    </div>
-    <div class="col-md-3">
-        <div class="mediumWhiteBold">Мы в социальных сетях</div>
-        <div>
-            <? Html::a(Html::img('@web/images/vk.svg', ['class' => 'moveUp']), 'https://vk.com/id433055831', ['title' => 'ВКонтакте']) ?>
-            <? Html::a(Html::img('@web/images/odnoklassniki.svg', ['class' => 'moveUp']), 'https://ok.ru/profile/571730537307', ['title' => 'Одноклассники']) ?>
-            <? Html::a(Html::img('@web/images/facebook.svg', ['class' => 'moveUp']), 'https://www.facebook.com/БУ-Няганская-городская-поликлиника-2189851657908954', ['title' => 'Facebook']) ?>
-            <? Html::a(Html::img('@web/images/twitter.svg', ['class' => 'moveUp']), 'https://twitter.com/profilngp1', ['title' => 'Twitter']) ?>
+<div id="footerHolder">
+    <footer class="container">
+        <div class="col-md-3">
+            <div class="mediumWhiteBold">Наименование</div>
+            <div class="smallWhite">Бюджетное учреждение Ханты-Мансийского автономного округа - Югры "Няганская городская поликлиника"</div>
         </div>
-    </div>
-    <div class="col-md-2">
-        <div class="mediumWhiteBold">Важная информация</div>
-        <div style="font-size: small">
-            Телефон единой службы спасения:
-            <span style="color: red; font-size: medium; font-weight: bold"><i><u>112</u></i></span>
+        <div class="col-md-2">
+            <div class="mediumWhiteBold">О поликлинике</div>
         </div>
-        <div style="font-size: small">
-            Контакт-центр: <span style="font-size: medium; font-weight: bold">8-800-100-86-03</span>
+        <div class="col-md-2">
+            <div class="mediumWhiteBold">Пациентам</div>
         </div>
-    </div>
-</footer>
+        <div class="col-md-2">
+            <div class="mediumWhiteBold">Социальные сети</div>
+            <div style="margin-top: 10px">
+                <?php $height = 30; ?>
+                <?= Html::a(Html::img('@web/images/vk.svg', ['height' => $height]), 'https://vk.com/id433055831', ['title' => 'ВКонтакте', 'style' => 'margin-right: 5px;']) ?>
+                <?= Html::a(Html::img('@web/images/odnoklassniki.svg', ['height' => $height]), 'https://ok.ru/profile/571730537307', ['title' => 'Одноклассники', 'style' => 'margin-right: 5px;']) ?>
+                <?= Html::a(Html::img('@web/images/facebook.svg', ['height' => $height]), 'https://www.facebook.com/БУ-Няганская-городская-поликлиника-2189851657908954', ['title' => 'Facebook', 'style' => 'margin-right: 5px;']) ?>
+                <?= Html::a(Html::img('@web/images/twitter.svg', ['height' => $height]), 'https://twitter.com/profilngp1', ['title' => 'Twitter']) ?>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="mediumWhiteBold">Важная информация</div>
+            <div class="smallWhite">
+                Телефон единой службы спасения:
+                <span class="mediumWhiteBold">112</span>
+            </div>
+            <div class="smallWhite">
+                Контакт-центр: <span class="mediumWhiteBold">8-800-100-86-03</span>
+            </div>
+        </div>
+    </footer>
+</div>
 
 <?php
     $this->registerCss('
-        footer {
+        #footerHolder {
             position: absolute;
-            bottom: 0;
+            bottom: 0px;
             height: 90px;
+            width: 100%;
+        }
+        footer {
+            height: 100%;
             background: rgb(51,51,51);
             padding: 5px;
             color: white;
