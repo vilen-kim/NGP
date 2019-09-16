7a5dbfa65901f76beecf05956a3fc921a8edf9e0
vilen-kim <vilen_kim@mail.ru>
2019-09-16 21:57:14 +0500

    Добавил на главную какой-то скрипт

--- views/site/index.php
+++ views/site/index.php
@@ -22,6 +22,16 @@ if (!Yii::$app->mobileDetect->isMobile()) {
 }
 ?>
 
+<script data-skip-moving="true">
+    (function (w, d, u) {
+        var s = d.createElement('script');
+        s.async = 1;
+        s.src = u + '?' + (Date.now() / 60000 | 0);
+        var h = d.getElementsByTagName('script')[0];
+        h.parentNode.insertBefore(s, h);
+    })(window, document, '//vi.admhmao.ru/assets/vi_widget/');
+</script>
+
 
 <div id="site-index" class="container">
 
